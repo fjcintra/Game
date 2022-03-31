a4d790df45b5e7e28f088f2572214577773d4987
Fabio Jose Cintra <88793715+fjcintra@users.noreply.github.com>
2022-03-31 19:34:50 -0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,31 @@
+from models.calcular import Calcular
+
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+def jogar(pontos: int) -> None:
+    dificuldade: int = int(input('Informe o nível de dficuldade desejado [1, 2, 3 ou 4]: '))
+
+    calc: Calcular = Calcular(dificuldade)
+
+    print('Informe o resultado para a seguinte operação:')
+    calc.mostrar_operacao()
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'Você tem {pontos} ponto(s).')
+
+    continuar: int = int(input('Deseja continuar jogando? (1 - sim, 0 - não) '))
+    if continuar:
+        jogar(pontos)
+    else:
+        print((f'Você finalizou com {pontos} pontos. \nObrigado por jogar!'))
+
+
+if __name__ == '__main__':
+    main()
+
